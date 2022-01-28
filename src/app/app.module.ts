de79844b4dcfa7896e3586d7678517c26c200d1d
Chitwan Bindal <chitwan001@gmail.com>
2022-01-28 22:25:19 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './MyComponents/header/header.component';
+import { SettingsComponent } from './MyComponents/settings/settings.component';
+import { HomeComponent } from './MyComponents/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SettingsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
